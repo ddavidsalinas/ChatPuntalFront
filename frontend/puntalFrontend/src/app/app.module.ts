0752374647c10b858051bc54fe1e6cc2db7a1e20
spuchades95 <spuchades95@gmail.com>
2024-01-14 14:54:08 +0100

    cards y cards con charts(en proceso)

--- frontend/puntalFrontend/src/app/app.module.ts
+++ frontend/puntalFrontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantillasComponent } from './plantillas/plantillas.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './card/card.component';
+import { CardConChartsComponent } from './card-con-charts/card-con-charts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlantillasComponent
+    PlantillasComponent,
+    DashboardComponent,
+    CardComponent,
+    CardConChartsComponent
   ],
   imports: [
     BrowserModule,
