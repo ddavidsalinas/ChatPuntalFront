1049ef799d4f11d9c52f89e5c7d7e6e8ad830831
spuchades95 <spuchades95@gmail.com>
2024-01-15 12:28:58 +0100

    x

--- frontend/puntalFrontend/src/app/chart/chart.component.ts
+++ frontend/puntalFrontend/src/app/chart/chart.component.ts
@@ -14,15 +14,17 @@ export class ChartComponent implements OnInit  {
   //"ctx" hace referencia al id del componente canvas
   
     const myChart = new Chart("ctx", {
-        type: 'pie',
+        type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','prueba','prueba2'],
             datasets: [{
                 label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [12, 19, 3, 5, 2, 3,9,7],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
@@ -32,6 +34,8 @@ export class ChartComponent implements OnInit  {
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
