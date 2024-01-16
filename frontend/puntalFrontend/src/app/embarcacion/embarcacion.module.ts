7b544a086f8ef7f8929365c51ac99dd8796345b4
Piterxus <piterxus@gmail.com>
2024-01-16 15:49:43 +0100

    Añado comp tabla embarcación

--- frontend/puntalFrontend/src/app/embarcacion/embarcacion.module.ts
+++ frontend/puntalFrontend/src/app/embarcacion/embarcacion.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TablaComponent } from './tabla/tabla.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TablaComponent
+  ],
   imports: [
     CommonModule
   ]
