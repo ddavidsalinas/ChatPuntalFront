072edf9d9719311bd8c0c02e5ae5c66c914cedd4
gabriel <glopez5@iesfuentesanluis.org>
2024-01-17 12:11:34 +0100

    elimar los antiguos componentes

--- frontend/puntalFrontend/src/app/app.module.ts
+++ frontend/puntalFrontend/src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantillasComponent } from './plantillas/plantillas.component';
-import { CardMovilComponent } from './card-movil/card-movil.component';
-import { PlantillaMovilComponent } from './plantilla-movil/plantilla-movil.component';
-import { CardVistaTransitoComponent } from './card-vista-transito/card-vista-transito.component';
 import { MovilModule } from './movil/movil.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlantillasComponent,
-    CardMovilComponent,
-    PlantillaMovilComponent,
-    CardVistaTransitoComponent
   ],
   imports: [
     BrowserModule,
