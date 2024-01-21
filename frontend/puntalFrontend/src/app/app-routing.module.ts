b44d0575d4ade490283dfc775b2ff46798b16d46
spuchades95 <spuchades95@gmail.com>
2024-01-21 14:22:28 +0100

    cosas raras

--- frontend/puntalFrontend/src/app/app-routing.module.ts
+++ frontend/puntalFrontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContenidoTransitoComponent } from './transito/contenido-transito/contenido-transito.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { EmbarcacionesComponent } from './embarcacion/embarcaciones/embarcaciones.component';
 
 const routes: Routes = [
   { path: 'transito/contenido-transito', component: ContenidoTransitoComponent },
-  { path: 'dashboard/dashboard', component: DashboardComponent}
+  { path: 'dashboard/dashboard', component: DashboardComponent},
+  { path: 'embarcacion/embarcaciones', component: EmbarcacionesComponent}
 
   
 ];
