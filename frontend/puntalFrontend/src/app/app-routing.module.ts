de940c12664a004775a6ae39cc17927353f1c4d8
piterxus <piterxus@gmail.com>
2024-01-24 11:15:24 +0100

    Enrutado a notificaciones ok

--- frontend/puntalFrontend/src/app/app-routing.module.ts
+++ frontend/puntalFrontend/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContenidoTransitoComponent } from './transito/contenido-transito/contenido-transito.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { EmbarcacionesComponent } from './embarcacion/embarcaciones/embarcaciones.component';
-
+import { NotificacionesComponent } from './notificaciones/notificaciones/notificaciones.component';
 const routes: Routes = [
   { path: 'transito/contenido-transito', component: ContenidoTransitoComponent },
-  { path: 'dashboard/dashboard', component: DashboardComponent},
-  { path: 'embarcacion/embarcaciones', component: EmbarcacionesComponent}
+  { path: 'dashboard/dashboard', component: DashboardComponent },
+  { path: 'embarcacion/embarcaciones', component: EmbarcacionesComponent },
+  { path: 'notificaciones', component: NotificacionesComponent }
 
-  
+
 ];
 
 @NgModule({
