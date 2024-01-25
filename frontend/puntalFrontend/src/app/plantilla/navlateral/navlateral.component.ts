fdf0c8044266b063922f0e4ca629a3920944a9a7
spuchades95 <spuchades95@gmail.com>
2024-01-25 23:47:43 +0100

    casi casi

--- frontend/puntalFrontend/src/app/plantilla/navlateral/navlateral.component.ts
+++ frontend/puntalFrontend/src/app/plantilla/navlateral/navlateral.component.ts
@@ -1,19 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-navlateral',
   templateUrl: './navlateral.component.html',
   styleUrls: ['./navlateral.component.css'],
 })
-export class NavlateralComponent {
-  dashboardRoute: string = '/dashboard/dashboard';
+export class NavlateralComponent implements OnInit {
+  rutaActiva: string = '';
+  claseBotonBloqueado: string = '';
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.obtenerRutaActiva();
+      }
+    });
+  }
+
+  obtenerRutaActiva() {
+   
+    const rutaActiva = this.route.snapshot.routeConfig?.path || '';
+    console.log(rutaActiva);
+    this.compararRuta(rutaActiva);
+  }
+
+  compararRuta(ruta: string) {
+    // Comparar la ruta con las rutas deseadas y realizar acciones en consecuencia
+    switch (ruta) {
+      case 'dashboard/dashboard':
+        claseBotonBloqueado: 'botonB';
+        break;
+      case '/transito/contenido-transito':
+        claseBotonBloqueado: 'botonB';
+        break;
+      case '/embarcaciones':
+        claseBotonBloqueado: 'botonB';
+        break;
+      case '/transito/tabla-transito':
+        claseBotonBloqueado: 'botonB';
+        break;
+      // Agregar más casos según sea necesario
+      default:
+        // Realizar acciones por defecto o según tus necesidades
+        break;
+    }
+  }
+
+
+
+
+  dashboardRoute: string = 'dashboard/dashboard';
   transitosRoute: string = '/transito/contenido-transito';
   embarcacionRoute: string = '/embarcaciones';
   transitos2Route: string = '/transito/tabla-transito';
 
 
+
+
   claseBotonDesbloqueado: string = 'botonA';
-  claseBotonBloqueado: string = 'botonB';
+
 
 
   imageUrlDashboardDesbloqueado = 'assets/img/control.svg';
