0d7fad563e338d316d3769ffb0740c554a90385a
spuchades95 <spuchades95@gmail.com>
2024-01-28 19:34:10 +0100

    estructura form

--- frontend/puntalFrontend/src/app/transito/transito.module.ts
+++ frontend/puntalFrontend/src/app/transito/transito.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContenidoTransitoComponent } from './contenido-transito/contenido-transito.component';
 import { TablaTransitoComponent } from './tabla-transito/tabla-transito.component';
+import { FormdialogoComponent } from './formdialogo/formdialogo.component';
 
 
 
 @NgModule({
   declarations: [
     ContenidoTransitoComponent,
-    TablaTransitoComponent
+    TablaTransitoComponent,
+    FormdialogoComponent
   ],
   imports: [
     CommonModule
