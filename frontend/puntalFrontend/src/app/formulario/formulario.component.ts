89f0f399104d764e7a82c79342c21d98550b12b5
David Salinas <davideosalinosa@gmail.com>
2024-01-31 13:19:26 +0100

    caca

---
+++ frontend/puntalFrontend/src/app/formulario/formulario.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input ,OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { datos } from 'src/resources/datos';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent 
+{
+  dtOptions: DataTables.Settings = {};
+  datos = datos.transitos;
+
+  @Input() input:any[]=[];
+  @Input() boton:string="";
+  @Input() texto:string="";
+  @Input() nombre:string="";
+  
+ 
+  
+  
+  
+}
+
