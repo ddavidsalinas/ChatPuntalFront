5aa01aa2485574fc79d1459b9b2ebd4271118192
Piterxus <piterxus@gmail.com>
2024-02-19 20:15:27 +0100

    Logout

--- frontend/puntalFrontend/src/app/plantilla/navheader/navheader.component.ts
+++ frontend/puntalFrontend/src/app/plantilla/navheader/navheader.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navheader',
@@ -7,4 +10,13 @@ import { Component, Input } from '@angular/core';
 })
 export class NavheaderComponent {
  name: string = 'x';
+  constructor(private authService: AuthService, private router: Router, private tokenService: TokenService) { }
+
+  logout() {
+    
+    this.tokenService.removeToken();
+
+    this.router.navigate(['/login']);
+  }
+
 }
