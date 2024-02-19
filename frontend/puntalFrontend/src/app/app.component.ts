d81a7113c56935610e83c76f55481770e170126b
piterxus <piterxus@gmail.com>
2024-02-19 11:40:39 +0100

    Parte front casi jwt

--- frontend/puntalFrontend/src/app/app.component.ts
+++ frontend/puntalFrontend/src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from './shared/token.service';
+import { AuthStateService } from './shared/auth-state.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isSignedIn!: boolean;
   title = 'puntalFrontend';
+  constructor( private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService){}
+
+    ngOnInit(): void {
+      this.auth.userAuthState.subscribe(val => {
+        this.isSignedIn = val;
+      });
+    }
+
+    signOut() {
+      this.token.removeToken();
+      this.auth.setAuthState(false);
+      this.router.navigate(['login']);
+    }
  
 }
