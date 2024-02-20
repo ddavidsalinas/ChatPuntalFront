b4a371785ee905fd59c17840233e3a4231bcf753
piterxus <piterxus@gmail.com>
2024-02-20 10:25:08 +0100

    Muestro rol desde log

--- frontend/puntalFrontend/src/app/login/login/login.component.ts
+++ frontend/puntalFrontend/src/app/login/login/login.component.ts
@@ -44,6 +44,7 @@ export class LoginComponent implements OnInit {
     this.authService.signin(this.loginForm.value).subscribe(
       (result) =>  {
         console.log('Resultado del login:', result);
+        console.log('Resultado del usuario:', result.user.Rol_id);
         this.responseHandler(result);
       },
       (error) => {
