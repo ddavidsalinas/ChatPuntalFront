d5f851d409256b49149b56ac6e971b588e976065
Gabriel Lopez <glopez5@iesfuentesanluis.org>
2024-02-25 22:52:46 +0100

    guardamuelles conectado con api transitos

--- frontend/puntalFrontend/src/app/movil/card-confirmacion-transito/card-confirmacion-transito.component.ts
+++ frontend/puntalFrontend/src/app/movil/card-confirmacion-transito/card-confirmacion-transito.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card-confirmacion-transito',
   templateUrl: './card-confirmacion-transito.component.html',
   styleUrls: ['./card-confirmacion-transito.component.css']
 })
-export class CardConfirmacionTransitoComponent {
+
+export class CardConfirmacionTransitoComponent implements OnInit {
+
+  transito: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.transito = history.state.transito;
+  }
 
 }
